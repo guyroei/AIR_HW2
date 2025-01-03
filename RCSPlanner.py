312bf6abf87de8f293fa3b27e07b15a063fbe9cc
guyroei <guyroei8@gmail.com>
2025-01-03 19:11:45 +0100

    Started RCS implementation

--- RCSPlanner.py
+++ RCSPlanner.py
@@ -17,6 +17,40 @@ class RCSPlanner(object):
         plan = []
 
         # TODO: Task 4
+        "Initialize coarse and fine sets with the desired actions"
+        coarse_set = []
+        fine_set = []
+        for num1 in [-2, 0, 2]:
+            for num2 in [-2, 0, 2]:
+                coarse_set.append((num1, num2))
+        coarse_set.remove((0, 0))
+        for num1 in [-1, 0, 1]:
+            for num2 in [-1, 0, 1]:
+                fine_set.append((num1, num2))
+        fine_set.remove((0, 0))
+
+        "node = (state, rank, resolution, parent_node)"
+        root_node = (self.planning_env.start, 0, "coarse", None)
+        open_list = [root_node]
+        close_list = []
+        while True:
+            "take the node with the minimal rank"
+            v = min(open_list, key=lambda node: node[1])
+            open_list.remove(v)
+            if self.planning_env.state_validity_checker(v[0]):
+                if v not in close_list:
+                    if (v[0] == self.planning_env.goal).all():
+                        self.reconstruct_path(v)
+                    for action in coarse_set:
+                        new_node = (v[0] + action, v[1] + 1, "coarse", v)
+                        open_list.append(new_node)
+                    close_list.append(v)
+            if (v != root_node) and (v[2] == "coarse"):
+                for action in fine_set:
+                    new_node = (v[3][0] + action, v[3][1] + 1, "fine", v[3])
+                    open_list.append(new_node)
+            if len(open_list) == 0:
+                break
 
         return np.array(plan)
 
